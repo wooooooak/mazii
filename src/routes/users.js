d1c8f16c12e302ad2e547d32a4ead6a11f74eba5
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2017-11-29 08:26:58 +0900

    마무리 정리 단

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 const Post = require('../model/Posts');
 const User = require('../model/Users');
 
@@ -17,37 +17,33 @@ router.get('/auth/facebook',(passport.authenticate('facebook',{
 // 패스포트 - 페이스북 인증 콜백 라우팅
 router.route('/auth/facebook/callback').get(passport.authenticate('facebook', {
   successRedirect : '/',
-  failureRedirect : '/profile'
+  failureRedirect : '/'
 }));
-
-//구글 소셜 로그인
-router.get('/auth/google',(passport.authenticate('google',{
-  scope:'email'
-})));
 // 패스포트 - 페이스북 인증 콜백 라우팅
 router.route('/auth/google/callback').get(passport.authenticate('google', {
   successRedirect : '/',
   failureRedirect : '/profile'
 }));
 
-//다른사람의 프로필을 눌렀을 경우
+//프로필을 눌렀을 경우
 router.get('/auth/profile/:email',(req,res)=>{
     let userEmail = req.params.email;
     console.log(userEmail);
     if(!req.params.email){
       userEmail = req.user.email;
     }
-    let otherUser; 
+    let profileUser; 
     User.findOne({'email':userEmail}).exec((err,user)=>{
       if(err) console.log(err);
-      otherUser = user;
-      console.log(otherUser);
+      profileUser = user;
+      console.log(profileUser);
     }).then((user)=>{
+      let emailId = getEmailId(user);
       res.render('profile.ejs',{
         user : req.user,
-        otherUser : user
+        profileUser : user,
+        emailId : emailId
       });
-
     });
 });
 
@@ -55,3 +51,10 @@ router.get('/auth/logout',(req,res)=>{
   req.logout();
   res.redirect('/');
 })
+
+function getEmailId(user){
+  let email = user.email;
+  console.log(email.indexOf('@')); 
+  let emailId = email.slice(0,email.indexOf('@'));
+  return emailId;
+}
